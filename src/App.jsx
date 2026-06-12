fe61c08218b11007d1353fcf097bccac883a87aa
niiquash <qua16006@byui.edu>
2026-06-11 23:34:20 -0600

    - Add layout component for more flexible rendering of nav and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
 import "./App.css";
-import Footer from "./components/footer/Footer";
-import Navbar from "./components/navbar/Navbar";
 import HomePage from "./pages/home-page/HomePage";
 import AboutPage from "./pages/about-page/AboutPage";
 import RecipesPage from "./pages/recipes-page/RecipesPage";
 import { Routes, Route } from "react-router";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <>
-      <Navbar />
       <Routes>
-        <Route index element={<HomePage />} />
-        <Route path="about" element={<AboutPage />} />
-        <Route path="recipes" element={<RecipesPage />} />
+        <Route element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="recipes" element={<RecipesPage />} />
+        </Route>
       </Routes>
-      <Footer />
     </>
   );
 }
