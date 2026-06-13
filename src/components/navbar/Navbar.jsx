eb1cabad43bbc3124b09a0275a6266295a819944
niiquash <qua16006@byui.edu>
2026-06-12 22:13:49 -0600

    - Fix bugs on menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,21 @@
 import HamburgerIcon from "../../assets/icon-hamburger-menu.svg";
 import Logo from "../../assets/logo.svg";
 import "./Navbar.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { NavLink, Link } from "react-router";
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
-  function handleMenuButtonClick() {
-    console.log("menu toggled!", toggleMenu);
+  const menuRef = useRef(null);
+
+  function handleMenuButtonClick(event) {
+    event.stopPropagation();
     setToggleMenu((prevToggleMenu) => !prevToggleMenu);
+  }
+
+  function handleMenuItemClick() {
+    setToggleMenu(false);
   }
 
   useEffect(() => {
@@ -23,12 +29,40 @@ const Navbar = () => {
     return () => window.removeEventListener("resize", handleWindowResize);
   }, []);
 
+  useEffect(() => {
+    function handleEscapeKey(event) {
+      if (event.key === "Escape") {
+        setToggleMenu(false);
+      }
+    }
+
+    document.addEventListener("keydown", handleEscapeKey);
+
+    return () => document.removeEventListener("keydown", handleEscapeKey);
+  }, []);
+
+  useEffect(() => {
+    function handleOutsideClick(event) {
+      console.log(menuRef.current.contains(event.target));
+      if (toggleMenu && !menuRef.current.contains(event.target)) {
+        setToggleMenu(false);
+      }
+    }
+
+    document.addEventListener("click", handleOutsideClick);
+
+    return () => document.removeEventListener("click", handleOutsideClick);
+  }, [toggleMenu]);
+
   return (
     <nav className="navigation">
       <Link to="/" className="logo-container">
         <img src={Logo} alt="An image of two flower petals." />
       </Link>
-      <div className={`menu-list-container ${toggleMenu ? "is-open" : ""}`}>
+      <div
+        ref={menuRef}
+        className={`menu-list-container ${toggleMenu ? "is-open" : ""}`}
+      >
         <ul className="menu-list">
           <li className="menu-list-item">
             <NavLink
@@ -36,6 +70,7 @@ const Navbar = () => {
                 isActive ? "menu-item-link active" : "menu-item-link"
               }
               to="/"
+              onClick={handleMenuItemClick}
             >
               Home
             </NavLink>
@@ -46,6 +81,7 @@ const Navbar = () => {
                 isActive ? "menu-item-link active" : "menu-item-link"
               }
               to="/about"
+              onClick={handleMenuItemClick}
             >
               About
             </NavLink>
@@ -56,6 +92,7 @@ const Navbar = () => {
                 isActive ? "menu-item-link active" : "menu-item-link"
               }
               to="/recipes"
+              onClick={handleMenuItemClick}
             >
               Recipes
             </NavLink>
