5ccfbf8f9286a8c305f7ee6f61d719d88717c50e
niiquash <qua16006@byui.edu>
2026-06-13 01:30:56 -0600

    - Complete hero section with image

--- src/pages/home-page/HomePage.jsx
+++ src/pages/home-page/HomePage.jsx
@@ -1,7 +1,36 @@
 import "./HomePage.css";
-
+import HomeHeroImage from "/image-home-hero-small.webp";
+import HeroSquiggle from "../../assets/pattern-squiggle-1.svg";
 const HomePage = () => {
-  return <div className="home-page-container">Home Page</div>;
+  return (
+    <main className="home-page-container">
+      <section className="hero-section">
+        <div className="hero-section-intro">
+          <h1 className="intro-heading">Healthy meals. zero fuss</h1>
+          <p className="intro-paragraph">
+            Discover eight quick whole-food recipes that you can cook
+            tonight&#8212;no processed junk, no guesswork.
+          </p>
+          <button className="hero-button">Start exploring</button>
+        </div>
+        <div className="hero-image-container">
+          <img
+            className="hero-image"
+            src={HomeHeroImage}
+            alt="A woman cooking in the kitchen."
+          />
+          <img
+            className="hero-squiggle"
+            src={HeroSquiggle}
+            alt="A blue squiggly line."
+          />
+        </div>
+      </section>
+      <section></section>
+      <section></section>
+      <section></section>
+    </main>
+  );
 };
 
 export default HomePage;
