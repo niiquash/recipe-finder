11fe8390f88068540960fcd57349483a9f1b2103
niiquash <qua16006@byui.edu>
2026-06-23 21:31:55 -0600

    - Create layout for recipe yield options

--- src/pages/recipes-page/RecipesPage.jsx
+++ src/pages/recipes-page/RecipesPage.jsx
@@ -23,12 +23,58 @@ const RecipesPage = () => {
                 Max Prep Time{" "}
                 <img src={ChevronDown} alt="Upside down carat symbol." />
               </button>
+              <div className="prep-time-options-and-reset-button">
+                <div className="prep-time-options">
+                  <div className="prep-time-option">
+                    <input type="radio" name="prep-time" id="0-minutes" />
+                    <label htmlFor="0-minutes">0 minutes</label>
+                  </div>
+                  <div className="prep-time-option">
+                    <input type="radio" name="prep-time" id="5-minutes" />
+                    <label htmlFor="5-minutes">5 minutes</label>
+                  </div>
+                  <div className="prep-time-option">
+                    <input type="radio" name="prep-time" id="10-minutes" />
+                    <label htmlFor="10-minutes">10 minutes</label>
+                  </div>
+                </div>
+                <button className="prep-time-reset-button" type="reset">
+                  Clear
+                </button>
+              </div>
             </div>
             <div className="cook-time-container">
               <button className="cook-time-button">
                 Max Cook Time{" "}
                 <img src={ChevronDown} alt="Upside down carat symbol." />
               </button>
+              <div className="cook-time-options-and-reset-button">
+                <div className="cook-time-options">
+                  <div className="cook-time-option">
+                    <input type="radio" name="cook-time" id="0-minutes" />
+                    <label htmlFor="0-minutes">0 minutes</label>
+                  </div>
+                  <div className="cook-time-option">
+                    <input type="radio" name="cook-time" id="5-minutes" />
+                    <label htmlFor="5-minutes">5 minutes</label>
+                  </div>
+                  <div className="cook-time-option">
+                    <input type="radio" name="cook-time" id="10-minutes" />
+                    <label htmlFor="10-minutes">10 minutes</label>
+                  </div>
+                  <div className="cook-time-option">
+                    <input type="radio" name="cook-time" id="15-minutes" />
+                    <label htmlFor="15-minutes">15 minutes</label>
+                  </div>
+                  <div className="cook-time-option">
+                    <input type="radio" name="cook-time" id="20-minutes" />
+                    <label htmlFor="20-minutes">20 minutes</label>
+                  </div>
+                </div>
+                <button className="cook-time-reset-button" type="reset">
+                  Clear
+                </button>
+              </div>
             </div>
           </div>
           <div className="search-bar">
